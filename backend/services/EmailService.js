15f9728b8eeb82959f88c3cb91bbbeb85ba6e2a5
santosh <santehero8@gmail.com>
2023-06-09 09:24:26 -0700

    env setup

--- backend/services/EmailService.js
+++ backend/services/EmailService.js
@@ -2,41 +2,41 @@ const nodemailer = require("nodemailer");
 const jwt = require("jsonwebtoken");
 const { hashPassword } = require("./AuthService");
 
-class EmailService{
+class EmailService {
 
-    async sendEmail({subject,text,html,email}){
+    async sendEmail({ subject, text, html, email }) {
 
 
 
-    let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true, 
-    auth: {
-        user: 'santehero8@gmail.com',
-        pass: 'vqok fser mcni vitr'
-    },
-  });
+        let transporter = nodemailer.createTransport({
+            host: "smtp.gmail.com",
+            port: 465,
+            secure: true,
+            auth: {
+                user: process.env.APP_EMAIL,
+                pass: process.env.SMTP_PW
+            },
+        });
 
-  try {
-    
-      let info = await transporter.sendMail({
-    from:process.env.APP_EMAIL, 
-    to: email, 
-    subject, 
-    text,
-    html, 
-});
+        try {
 
-  console.log("Message sent: %s", info.messageId);
-  return info.messageId;
+            let info = await transporter.sendMail({
+                from: process.env.APP_EMAIL,
+                to: email,
+                subject,
+                text,
+                html,
+            });
 
-} catch (error) {
-    return error.message;
-}
+            console.log("Message sent: %s", info.messageId);
+            return info.messageId;
+
+        } catch (error) {
+            return error.message;
+        }
     }
 
-    async confirmationEmail({text,subject,html,email}){
+    async confirmationEmail({ text, subject, html, email }) {
         const emailPayload = {
             text,
             subject,
@@ -44,55 +44,55 @@ class EmailService{
             email,
         }
         try {
-              const messageId =   await this.sendEmail(emailPayload)
-              return messageId;
+            const messageId = await this.sendEmail(emailPayload)
+            return messageId;
 
         } catch (error) {
             console.log(error)
-            return error;            
+            return error;
 
         }
 
     }
 
 
-       createEmailConfirmationHash(email){
+    createEmailConfirmationHash(email) {
 
 
 
-     const hash =    this.createEmailToken(email);
-     
-     return hash;
-    
-  }
+        const hash = this.createEmailToken(email);
 
-   createEmailToken(email){
- 
-        const token = jwt.sign({email},process.env.CONFIRMATION_HASH,{expiresIn:60*10})
-         return token;
+        return hash;
 
-   
-  }  
-
-  async verifyEmailConfirmationToken(token){
-
-    try {
-        
-        const decoded =  jwt.verify(token,process.env.CONFIRMATION_HASH)
-
-
-        console.log("the decoded",decoded);
-        
-        return {email:decoded?.email, exp:false, invalidLink:false}
-    } catch (error) {
-        console.log(error.message)
-        if(error?.message==="jwt expired"){
-           return {email:null, exp:true, invalidLink:false}
-        }else{
-            return {email:null, exp:false, invalidLink:true}
-        }
-            
     }
+
+    createEmailToken(email) {
+
+        const token = jwt.sign({ email }, process.env.CONFIRMATION_HASH, { expiresIn: 60 * 10 })
+        return token;
+
+
+    }
+
+    async verifyEmailConfirmationToken(token) {
+
+        try {
+
+            const decoded = jwt.verify(token, process.env.CONFIRMATION_HASH)
+
+
+            console.log("the decoded", decoded);
+
+            return { email: decoded?.email, exp: false, invalidLink: false }
+        } catch (error) {
+            console.log(error.message)
+            if (error?.message === "jwt expired") {
+                return { email: null, exp: true, invalidLink: false }
+            } else {
+                return { email: null, exp: false, invalidLink: true }
+            }
+
+        }
     }
 
 }
