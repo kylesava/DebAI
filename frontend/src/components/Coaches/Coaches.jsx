692c3d83daf5549059ba382c21043242e86abec2
santosh <santehero8@gmail.com>
2023-06-13 22:43:32 -0700

    coaches box

--- frontend/src/components/Coaches/Coaches.jsx
+++ frontend/src/components/Coaches/Coaches.jsx
@@ -1,4 +1,5 @@
 import styles from "./coaches.module.css"
+import {HiRectangleGroup} from "react-icons/hi2"
 
 const Coaches = () => {
   return (
@@ -20,9 +21,12 @@ const Coaches = () => {
 
             <img src="/images/coach1.png" alt="coach" />
           </div>
+          <div className={styles.coach_bottom_content}>
             <p className={styles.coach_desc}> <b>Olivia : </b> Oxford graduate and DebAi coach, empowering minds with expertise.</p>
+            
           
-            <button className={styles.coach_hire_button}>Hire </button>
+            <button className={styles.coach_hire_button}>   <HiRectangleGroup/>  Hire </button>
+          </div>
         </div>
         <div className={styles.coach_box}>
         <div className={styles.coach_img_wrapper}>
@@ -33,7 +37,7 @@ const Coaches = () => {
 
             <p className={styles.coach_desc}> <b>Rachel : </b> Oxford graduate and DebAi coach, empowering minds with expertise.</p>
           
-            <button className={styles.coach_hire_button}>Hire </button>
+            <button className={styles.coach_hire_button}> <HiRectangleGroup/> Hire </button>
         </div>
         </div>
 
